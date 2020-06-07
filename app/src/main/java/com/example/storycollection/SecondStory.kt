fb0f56ac7d8eef153599c5289dc157656ed61afb
kaitha9 <kaithamanish@gmail.com>
2020-06-07 11:23:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/storycollection/SecondStory.kt
@@ -0,0 +1,12 @@
+package com.example.storycollection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondStory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_story)
+    }
+}
